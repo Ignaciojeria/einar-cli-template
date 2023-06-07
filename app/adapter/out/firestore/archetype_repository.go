0478022adffcf10c7ecb9f6f528a29f95b0cfd3a
Ignacio <ignaciovl.j@gmail.com>
2023-06-07 11:43:06 +0000

    improving template for allowing out ports generation if a component requires

--- app/adapter/out/firestore/archetype_repository.go
+++ app/adapter/out/firestore/archetype_repository.go
@@ -2,9 +2,15 @@ package out
 
 import (
 	"archetype/app/ports"
+	fs "archetype/app/shared/archetype/firestore"
 	"context"
+
+	"cloud.google.com/go/firestore"
 )
 
 var ArchetypeRepository ports.Out = func(ctx context.Context, REPLACE_BY_YOUR_DOMAIN map[string]string) error {
+	var _ *firestore.CollectionRef = fs.Client.Collection("INSERT_YOUR_COLLECTION_PATH_HERE")
+	//PUT YOUR FIRESTORE OPERATION HERE :
+	//....
 	return nil
 }
