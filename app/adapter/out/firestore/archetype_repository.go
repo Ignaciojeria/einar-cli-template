c1fb3b8dcf0cf7b01464e4a73c36d235fba95e5e
Ignacio <ignaciovl.j@gmail.com>
2023-07-15 05:12:01 +0000

    firestore template improvements

--- app/adapter/out/firestore/archetype_repository.go
+++ app/adapter/out/firestore/archetype_repository.go
@@ -4,15 +4,10 @@ import (
 	"archetype/app/domain/ports/out"
 	einar "archetype/app/shared/archetype/firestore"
 	"context"
-	"fmt"
 )
 
 var ArchetypeRepository out.ArchetypeOutBoundPort = func(ctx context.Context, REPLACE_BY_YOUR_DOMAIN map[string]string) error {
 	collectionReference := einar.Client.Collection("INSERT_YOUR_COLLECTION_CONSTANTS_HERE")
-	fmt.Println(collectionReference)
-	//PUT YOUR FIRESTORE OPERATION HERE :
-	//....collectionReference.Doc()
-	//....collectionReference.Add()
-	//....
+	_ = collectionReference
 	return nil
 }
