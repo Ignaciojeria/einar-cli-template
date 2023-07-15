556728312e0cfb58268c6542ecc5bd7cad8b5f95
Ignacio <ignaciovl.j@gmail.com>
2023-07-15 04:45:07 +0000

    firestore repository improvements

--- app/adapter/out/firestore/archetype_repository.go
+++ app/adapter/out/firestore/archetype_repository.go
@@ -2,29 +2,17 @@ package firestore
 
 import (
 	"archetype/app/domain/ports/out"
-	"archetype/app/shared/archetype/container"
 	einar "archetype/app/shared/archetype/firestore"
 	"context"
-
-	"cloud.google.com/go/firestore"
-	"github.com/google/uuid"
+	"fmt"
 )
 
-var archetypeCollection *firestore.CollectionRef
-
-func init() {
-	container.InjectOutboundAdapter(func() error {
-		archetypeCollection = einar.Client.Collection("INSERT_YOUR_COLLECTION_CONSTANTS_HERE")
-		return nil
-	}, container.InjectionProps{
-		DependencyID: uuid.NewString(),
-	})
-}
-
 var ArchetypeRepository out.ArchetypeOutBoundPort = func(ctx context.Context, REPLACE_BY_YOUR_DOMAIN map[string]string) error {
+	collectionReference := einar.Client.Collection("INSERT_YOUR_COLLECTION_CONSTANTS_HERE")
+	fmt.Println(collectionReference)
 	//PUT YOUR FIRESTORE OPERATION HERE :
-	//....archetypeCollection.Doc()
-	//....archetypeCollection.Add()
+	//....collectionReference.Doc()
+	//....collectionReference.Add()
 	//....
 	return nil
 }
