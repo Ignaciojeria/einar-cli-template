c2e0f3956e3b06ff7b054cfa88fa9d5d88778ee4
Ignacio <ignaciovl.j@gmail.com>
2023-07-15 05:17:06 +0000

    semantics improvementes

--- app/adapter/out/firestore/archetype_repository.go
+++ app/adapter/out/firestore/archetype_repository.go
@@ -4,10 +4,11 @@ import (
 	"archetype/app/domain/ports/out"
 	einar "archetype/app/shared/archetype/firestore"
 	"context"
+
+	"cloud.google.com/go/firestore"
 )
 
 var ArchetypeRepository out.ArchetypeOutBoundPort = func(ctx context.Context, REPLACE_BY_YOUR_DOMAIN map[string]string) error {
-	collectionReference := einar.Client.Collection("INSERT_YOUR_COLLECTION_CONSTANTS_HERE")
-	_ = collectionReference
+	var _ *firestore.CollectionRef = einar.Client.Collection("INSERT_YOUR_COLLECTION_CONSTANT_HERE")
 	return nil
 }
