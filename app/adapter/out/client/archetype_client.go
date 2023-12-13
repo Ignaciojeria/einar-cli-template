cbbcb74877338c7a3076a29455698c305414e8d5
ignaciojeria <ignaciovl.j@gmail.com>
2023-12-13 16:41:00 -0300

    http component trace mechanism improved

--- app/adapter/out/client/archetype_client.go
+++ app/adapter/out/client/archetype_client.go
@@ -1,66 +1,53 @@
 package client
 
 import (
-	einar "archetype/app/shared/archetype/resty"
-	"context"
 	"archetype/app/exception"
-	"archetype/app/shared/archetype/slog"
+	einar "archetype/app/shared/archetype/resty"
 	"archetype/app/shared/constants"
+	"context"
+	"errors"
+
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/trace"
 )
 
-var ArchetypeRestyClient = func(ctx context.Context, REPLACE_BY_YOUR_DOMAIN map[string]string) (err error) {
-	
+var ArchetypeRestyClient = func(ctx context.Context, REPLACE_BY_YOUR_DOMAIN map[string]string) error {
 	req := einar.Client.NewRequest().SetContext(ctx)
-
 	//Replace Get by your http method
 	res, err := req.Get("http://localhost:8080/api/ping")
 
 	ctx, span := tracer.Start(ctx, "ArchetypeRestyClient")
-	defer span.End()
+	defer func() {
+		if err != nil {
+			span.SetStatus(codes.Error, err.Error())
+			if errors.Is(err, exception.HTTP_NETWORK_ERROR) {
+				span.RecordError(err, trace.WithAttributes(
+					attribute.String(constants.ResponseBody, constants.NoResponse),
+				))
+			}
+			span.RecordError(err, trace.WithAttributes(
+				attribute.String(constants.ResponseBody, string(res.Body())),
+			))
+		}
+		span.End()
+	}()
 
 	if err != nil {
-		networkErr := exception.HTTP_NETWORK_ERROR
-		slog.
-			SpanLogger(span).
-			Error(networkErr.Error(),
-				constants.Error, err.Error())
-		span.SetStatus(codes.Error, networkErr.Error())
-		span.RecordError(networkErr, trace.WithAttributes(
-			attribute.String(constants.ResponseBody, constants.NoResponse),
-		))
-		return networkErr
+		err = exception.HTTP_NETWORK_ERROR
+		return err
 	}
 
 	if res.StatusCode() >= 500 && res.StatusCode() <= 599 {
-		externalServerErr := exception.EXTERNAL_SERVER_ERROR
-		slog.
-			SpanLogger(span).
-			Error(externalServerErr.Error(),
-				constants.ResponseBody, res.Body())
-		span.SetStatus(codes.Error, externalServerErr.Error())
-		span.RecordError(externalServerErr, trace.WithAttributes(
-			attribute.String(constants.ResponseBody, string(res.Body())),
-		))
-		return externalServerErr
+		err = exception.EXTERNAL_SERVER_ERROR
+		return err
 	}
 
 	if res.StatusCode() > 299 {
-		externalUnknownErr := exception.EXTERNAL_UNKNOWN_ERROR
-		slog.
-			SpanLogger(span).
-			Error(externalUnknownErr.Error(),
-				constants.ResponseBody, res.Body())
-		span.SetStatus(codes.Error, externalUnknownErr.Error())
-		span.RecordError(externalUnknownErr, trace.WithAttributes(
-			attribute.String(constants.ResponseBody, string(res.Body())),
-		))
-		return externalUnknownErr
+		err = exception.EXTERNAL_UNKNOWN_ERROR
+		return err
 	}
 
 	//Handle successfull response Here
-
 	return nil
 }
